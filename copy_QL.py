227ca88f79af628dd24485542c5909175902424a
Yonghui Tao <ytao624@aucklanduni.ac.nz>
2019-01-21 16:07:42 +1300

    ql

--- copy_QL.py
+++ copy_QL.py
@@ -1,17 +1,22 @@
 import numpy as np
-import pandas as pd
-
 
 class copy_QL:
-    def __init__(self, actions):
-        self.q_table = pd.DataFrame(columns = self.actions, dtype = np.float64)
+    def __init__(self, actions, states):
+        self.q_table = np.zeros(states, actions)
         self.actions = actions
         self.alpha = 0.01
         self.gamma = 0.95
         self.epsilon = 0.9
 
-
     def choose_action(self, observation):
         
+        if np.random.rand() > self.epsilon:
+            action = np.random.choice(self.actions)
+
+        else:
+            action = np.argmax(self.actions)
+
+
         
+
 
