1bbdde23ef6e996ba60f6eef7b03c9a8b378318e
Iqra <iahm612@aucklanduni.ac.nz>
2019-01-23 12:47:29 +1300

    Auto stash before merge of "master" and "origin/master"
    commenting

--- run-Roulette.py
+++ run-Roulette.py
@@ -1,17 +1,25 @@
+
 import gym
 import numpy as np
 # from /home/lemon740/Gym-T4-Testbed import Roulette
+#this is importing the algorithm which is defined as a class from a diffeerent folder
 from Roulette import QLearning
 import os
 import time
 import sys
 
+#our bash file takes the arg.txt file that has a list of the different environments episodes and steps
+#when using sys.argv start by using 1 not 0 for the first agrument because 0 will be the python file name
 EPISODE_NUM = int(sys.argv[2])
 STEP_NUM = int(sys.argv[3])
 
 env = gym.make(sys.argv[1])
+#action space=38
+#state =1
 actions=env.action_space.n
 states=env.observation_space.n
+# the class doesn't take in any parameters but to pass in the action and state space in your class
+#  use the name __init__ with double underscores
 QLearning = QLearning(states, actions)
 
 for episode in range(EPISODE_NUM):
@@ -24,14 +32,19 @@ for episode in range(EPISODE_NUM):
         # os.system('clear')
         # time.sleep(0.1)
 
+        # roulete doesn't have env.render
+
+        #each step you choose action by calling the choose action function in our algorithm file
         action = QLearning.choose_action(observation,EPISODE_NUM, env)
-        # print(action)
+
+        # apply action
         observation_, reward, done, _ = env.step(action)
 
+        #learn from the action(updating q values)
         QLearning.learn(observation, action, reward, observation_, done)
-
+        #get cumalative reward
         episode_rewards += reward
-
+        #move on to next state
         observation = observation_
 
         if done:
