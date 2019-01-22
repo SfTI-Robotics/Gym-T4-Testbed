e19aa8d9d651927b5e7913f09b683a0ba96bfb1e
Dweep Kapadia <dweep.kapadia@gmail.com>
2019-01-22 16:21:36 +1300

    Created first Prototype for TestBench
    Instructions are in the comments of the Testbench, Can run very simply Copyv0 code

--- copy_v0_easy.py
+++ copy_v0_easy.py
@@ -1,10 +1,27 @@
+
+import sys
 import gym
 from gym import wrappers
-env = gym.make('Copy-v0')
-env = wrappers.Monitor(env, '/tmp/copy-v0', force=True)
-for i_episode in range(500):
+
+
+# Change the the gym environments into the Command line inputs
+# sys.argv is a list of commandline argumenets
+
+#Making enum to make it readable in the code
+ENVIRONMENT = sys.argv[1]
+EPISODES = int(sys.argv[2])
+STEPS = int(sys.argv[3])
+
+env = gym.make(ENVIRONMENT)
+
+# 'Copy-v0' , Place command line argument when running the file
+#Providing 3 command line arugmenets (Env, Episodes , Steps)
+env = wrappers.Monitor(env, '/tmp/' + ENVIRONMENT, force=True)
+
+
+for i_episode in range(EPISODES):
     observation = env.reset()
-    for t in range(100):
+    for t in range(STEPS):
         action = (1,1,observation)
         observation, reward, done, info = env.step(action)
         if done:
