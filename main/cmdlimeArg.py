e3f614c3fda6b93b0a077a921728b44430276e9e
Yonghui Tao <ytao624@aucklanduni.ac.nz>
2019-02-04 10:31:11 +1300

    main

--- main/cmdlimeArg.py
+++
@@ -1,9 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-network", help="select a network type")
-parser.add_argument("-brain", help="select a brain type")
-parser.add_argument("-preprocess", help="select a preprocess type")
-args = parser.parse_args()
-
-
-print(args.network)
