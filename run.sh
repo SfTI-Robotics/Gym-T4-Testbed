c1fda4c501975ececba006055960b32088f4e295
K Lee <kevinle2997@gmail.com>
2019-02-13 11:58:25 +1300

    rewrite bash

--- run.sh
+++ run.sh
@@ -1,46 +1,5 @@
 #!/bin/bash
 
-# Read the textfile which holds the arguments and then parse those arguments 
-# into the run file for an algorithm
-
-
-#it reads the file per line, IFS leaves in the whitespacing
-# [[ -n "$Line" ]] -> this prevents the last line from being ignored"
-
-
-#I set the IFS to tab spaces that splits based on tabs per line
-while IFS=$'\t' read -r line || [[ -n "$line" ]];
-
-#loop
-do 
-	#Echo is used to print so , printing the line
-	words=($line)
-
-	#print the array of words
-	
-	echo "Now Running ${words[0]} environment"
-	
-	sleep 1s
-
-	#echo "${words[0]}"
-	#echo "${words[1]}"
-	#echo "${words[2]}"
-
-	echo "Inputting argument parameters"
-
-	sleep 1s
-
-
-	#Running the python script
-	#hard code the link 
-	# python3 copy_v0_easy.py ${words[0]} ${words[1]} ${words[2]}
-	python3 run-Roulette.py ${words[0]} ${words[1]} ${words[2]}
-
-
-# checking if the entire file has been readb
-done < "$1"
-
-
-
-#Run the bash file ./NAME_OF_BASH_FILE
-#if you cant run this just go chmod +x the_file_name
+# list the testing algorithm, environment and episode number 
+python3 run_main.py -alg DQN -env Pong-v0 -eps 10000
+# python3 run_main.py -alg DQN -env Pong-v0 -eps 10000
