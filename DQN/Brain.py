f6edea513ba12ad6f7ff5b595251f19f663a315f
K Lee <kevinle2997@gmail.com>
2019-02-12 16:46:39 +1300

    cleaned up Pong-v0

--- DQN/Brain.py
+++ DQN/Brain.py
@@ -1,13 +1,21 @@
 import numpy as np
 import random
 from collections import deque
+# import the network class from file 
 from DQN.Network import neural_net
 import time 
 
+# ========================
+
+# length of transitions deque
 MAX_MEMORY_LENGTH = 6000
+# alpha
 LEARNING_RATE = 0.1
+# gamma
 REWARD_DECAY = 0.8	
-START_TRAINING = 5000
+#used for memory replay see comment below
+START_TRAINING = 5000  
+# how many memory's we learn from at a time
 batch_size=32
 
 class Learning():
@@ -16,125 +24,55 @@ class Learning():
     def __init__(self,observations, actions):
         self.state_space = observations 
         self.action_space = actions
-
+        #initialise network here and not in the main file
+        #length of action space should be whatever new_action_space outputs see preprocess for more
         self.network = neural_net(self.state_space, self.action_space)
-        
+        #initialiseing epsilon changes immmediatley
         self.epsilon = 0
         self.gamma = REWARD_DECAY
         self.alpha = 0.86
         #transitions is where we store memory of max memory length
         self.transitions = deque(maxlen = MAX_MEMORY_LENGTH)
 
-    # the processed state is used in choosing action
+    
     def choose_action(self, state, episode):
         if random.random() > self.epsilon:
+            #this expolres by choosing a randomised action
             action = random.randrange(self.action_space)
-            print('EXPLORE')
+            # print('EXPLORE')
         else:
+            #this exploits by choosing your max of your calculated q values
             action = np.argmax(self.network.model.predict(np.expand_dims(state, axis = 0)))
-            ACT_VALUES=self.network.model.predict(np.expand_dims(state, axis = 0))
-            # action = np.argmax(ACT_VALUES[0])
-            # if self.network.model.predict(np.expand_dims(state, axis = 0)).any() < 0:
-            # print('episode ', episode, ' has negative')
-            # print('Episode ', episode, ': q values:  ', ACT_VALUES)
-            # print('Episode ', episode, ': action:  ', action)
-            print('EXPLOIT')
+            # print('EXPLOIT')
 
-        # decay epsilon
-        # if episode > 3000:
-        	# self.epsilon = 1 - 3 ** (-0.00023 * (episode - 3000))
-        # self.epsilon = 1 - 3 ** (-0.00023 * episode)
-        
         # increase epsilon
+        # equation designed for training on 10 000 episodes
+        # epsilon is below 0 until 2000 episodes is reached and is approx 1 for last 1000 episodes
         self.epsilon = 1 - 1.28 ** (-0.003 * (episode - 2000))
-        # self.epsilon = 1 - 1.4 ** (-0.003 * (1/episode))
+
         return action
-
-    # def back_propagate(self):
-
-
 
 
     def memory_replay(self):
-        # experience replay learning from our memories
+        # experience replay learning from our memories once there are 5000 memories
+        # condition for how many transitions need to stored before meory replay is used(1 step=1 transtion)
         if len(self.transitions) < START_TRAINING:
             return
 
-        # if len(self.transitions) > MAX_MEMORY_LENGTH:
-        #     sample_index = np.random.choice(self.memory_size, size=self.batch_size)
-        # else:
-        #     sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
-        # batch = self.memory[sample_index, :]
-
         # experience replay
+        # randomly select 32 memories from 5000
         batch = random.sample(self.transitions, batch_size)
-        # print(np.shape(self.transitions))
-        # print('batch states =', batch[0][0])
-        # print('batch actions =', batch[0][1])
-        # print('batch rewards =', batch[0][2])
-        # print('batch next state =', batch[0][3])
-        # print('batch dones =', batch[0][4])
-        #print(np.shape(batch))
-        ###############################################################################################
-        # initialise arrays
-
-    #     states = np.zeros((batch_size, *self.state_space)) 
-    #     next_states = np.zeros((batch_size, *self.state_space))
-    #     action, reward, done = [], [], []
-
-    # #     # extract variables from transition
-    # #     # extract seperate s,a,r.s'
-    #     for i in range(batch_size):
-    #         states[i] = batch[i][0]
-    #         action.append(batch[i][1])
-    #         reward.append(batch[i][2])
-    #         next_states[i] = batch[i][3]
-    #         done.append(batch[i][4])  
-
-    #         target = self.network.model.predict(states, batch_size=batch_size)
-    #         target_next = self.network.model.predict(next_states, batch_size=batch_size)
-    # #         # print(np.shape(target))
-    # #         # time.sleep(10)
-    # # ###############################################################################################
-
-    #     for sample in range(batch_size):
-    #         # check if transition was at end of episode
-    #         is_done = done[sample]
-    #         if is_done:
-    #             target[sample][action[sample]] = reward[sample]
-    #         else:
-    #             # Bellman Equation
-    #             target[sample][action[sample]] = reward[sample] + self.gamma * np.max(target_next[sample])
         
-    #         #                                      A   
-    #         # [Obseravations -> [predicted , R or R + predicted% , predicted]]
-
-    #         # calculates loss and does optimisation
-    #         # run graph
-    #         # Predicted Q -> Actual Q
-    #         n = np.expand_dims(states[sample], axis= 0)
-    #         # print(np.shape(n))
-    #         # print(np.shape(target[sample]))
-    #         # print(target[sample])
-    #         # a = np.expand_dims(target[sample], axis= 1)
-    #         # self.network.model.fit(n, target[sample],batch_size = batch_size, verbose=0)
-
-    #         self.network.model.fit(states, target,batch_size = batch_size, verbose=0)
-
-
-        # ======================================================================================
-
         for state, action, reward, next_state, done in batch:
             target = reward
             if not done:
+                # resize array by increasing dimension
                 next_state = np.expand_dims(next_state, axis= 0)
-                #print('np.shape(next_state) =', np.shape(next_state) )
-                # print('next_state) =',  np.expand_dims(next_state, axis= 0))
+                # bootstrapping the predicted reward as Q-value
                 target = reward + self.gamma * np.max(self.network.model.predict(next_state))
 
-
+            # resize array by increasing dimension
             state = np.expand_dims(state, axis=0)
-            #print('np.shape(state)', np.shape(state))
             target_f = self.network.model.predict(state)
             
             target_f[0][action] = target
--- Preprocess/Pong_Preprocess.py
+++ Preprocess/Pong_Preprocess.py
@@ -1,76 +1,94 @@
 from collections import deque
-from skimage import transform # Help us to preprocess the frames
 from skimage.color import rgb2gray # Help us to gray our frames
 import numpy as np
+#uncomment if you want to see what you've process and if it getting the right input
 # import cv2
 import time
 
 
 class Processing:
     def __init__(self):
+        # deque is used to temporaily hold images while it being stacked        
         self.deque = deque([np.zeros((80,80), dtype=np.int) for i in range(2)], maxlen=2)
-        self.step_max = 300
+        # This stuff is need for the graph this needs to be set up for each environment
+        # We decided to put it in preprocess as for each environment:
+        #                                                            the range of steps a game takes changes 
+        #                                                            but the lowest is always zero so only max is entered here
+        #                                                            same for time min will always be zero while time max varies from env to env
+        #                                                            reward min and max both vary        
+        self.step_max = 3000
         self.time_max = 40
         self.reward_min = -35                        
         self.reward_max= 35
 
     def Preprocessing(self, frame, is_new_episode):
-        # grayscale
+        # this is to see if your processedframed looks alright
+        # the part below show the original frame        
         # cv2.imshow("image", frame)
-       #
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
+
+        # grayscale
         frame = rgb2gray(frame)
         
+        #cropping frame
+        # this is specific to pong and the main reason we decided to 
+        # set up a proccesing file instaed of doing it in the main
         frame = frame[35:195]        
 
-        # frame[frame == 144] = 0 # erase background (background type 1)
-        # frame[frame == 109] = 0 # erase background (background type 2)
-        # frame[frame != 0] = 1 # everything else (paddles, ball) just set to 1
 
         # frame = frame / 255.0
+        # extract every second pixel in the image and resizes it to 80 x 80 
         frame = frame[::2, ::2]
-        # frame = transform.resize(frame,[160,160])
+        
+        
+        # uncomment to see single processed frame
     #     cv2.imshow("image", frame)
-    #    #
     #     cv2.waitKey(0)
     #     cv2.destroyAllWindows()
-
-        state = self.four_frames_to_state(frame, is_new_episode)
-        #img = cv2.imread(state,0)
+        
+        # state is a deque of 2 frames
+        state = self.frames_to_state(frame, is_new_episode)
         
         return state
 
-      
-        # return frame.astype(np.float).ravel()
-
-
-    def four_frames_to_state(self, frame, is_new_episode):
+    def frames_to_state(self, frame, is_new_episode):
         
         if is_new_episode:
             # all frames in new deque are of same state
+            # new epsisode the deque is completely empty so both slots need to be filled
             self.deque.append(frame)
             self.deque.append(frame)
-            # self.deque.append(frame)
-            # self.deque.append(frame)
 
         else:
+            # append new frame to deque
+            # in other steps one slot is already filled and the second one needs to be filled
             self.deque.append(frame)
-        # reshape the deque
+            
+        # reshape the deque so that network recognises that its 4 frames stacked toghether
         stacked_state = np.stack(self.deque, axis = 0)
         return stacked_state
 
     def get_state_space(self):
-        print('image size: ', np.shape(self.deque))
+        # this is used to return the processed shape  to the network initialiser
+        # print('image size: ', np.shape(self.deque))
         return np.shape(self.deque) 
-        # return (2,80,80)
 
-        # return 80*80
+
+# In Pong rewards can be {-1,0,1}
+# when -1 or 1 game has been reset
+# the reward is backpropagated so that the actions leading up to the result 
+# are assigned a percentage of the reward.
+# 'running add' variable that resets to 0 whenever this happens
+
 
     def discounted_rewards(self, rewards, gamma):
-        # initialise array
+        # initialise array and variable
         discounted_r = np.zeros_like(rewards, dtype=np.float32)
+        # np.zeroes_like(rewards) creates an array the same shape as the input
         running_add = 0
+
+        # starting from the last element, the reward of each action is changed to be a percentage of 1 or -1
         for i in reversed(range(len(rewards))):
             if rewards[i] != 0:
                 running_add = 0
@@ -79,17 +97,26 @@ class Processing:
 
         return discounted_r
 
-    # according to the resource below, actions come in pairs, 
+    # according to the resource below, Pong-v0 actions come in pairs, 
     # therefore, we choose to only pass in one of each pair.
     # this results in only threee actions
     # https://ai.stackexchange.com/questions/2449/what-are-different-actions-in-action-space-of-environment-of-pong-v0-game-from
 
     def new_action_space(self,actions):
         # out of 6 actions, only use 3
+        # 0 and 1 is taking no move
+        # 2 and 4 move up 
+        # 3 and 5 move down
+        # we want it to only be able to select 0,2,3
         return 3 
 
     def mapping_actions_to_keys(self,action_taken):
         # maps the 3 unique actions to OpenAI's gym action space (6 actions)
+        # the actions it takes from main are 0-2
+        # 0=0(no move)
+        # 1=2(up)
+        # 2=3(down)
+
         if action_taken == 0:
             action = 0
         elif action_taken == 1:
