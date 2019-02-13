6722ae7860afc1992e6a1c8d04f0224bfdc63622
Yonghui Tao <ytao624@aucklanduni.ac.nz>
2019-02-13 16:50:14 +1300

    testing for recording-only 4 games per episode

--- framework-gif.py
+++ framework-gif.py
@@ -8,21 +8,18 @@ import os
 
 
 env = gym.make('Pong-v0')
-# env.monitor.start('/tmp/cartpole-experiment-1')
 
 observation = env.reset()
-# env = gym.wrappers.Monitor(env,directory='pong/',force=True,write_upon_reset=True)
 for i_episode in range(20):
-    observation = env.reset()
-#     env = gym.wrappers.Monitor(env,directory='pong/',force=True,write_upon_reset=True)
-    for t in range(100):
-        env.render()
-     #    print observation
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print( "Episode finished after {} timesteps".format(t+1))
-            env = gym.wrappers.Monitor(env,directory='/Gym-T4-Testbed/temp_Videos/pong/',force=True,write_upon_reset=True)
-            break
+   observation = env.reset()
+   for t in range(3000):
+      env.render()
+      action = env.action_space.sample()
+      observation, reward, done, info = env.step(action)
+      if done:
+         print( "Episode finished after {} timesteps".format(t+1))
+         # env = gym.wrappers.Monitor(env,directory='Videos/pong/',video_callable=lambda episode_id: True, force=True,write_upon_reset=True)
+         env = gym.wrappers.Monitor(env,directory='Videos/pong/',video_callable=lambda episode_id: True, force=True,write_upon_reset=False)
+         break
 
 env.env.close()
