04b1f922a2b81ad4d684ea85fea63980daf0c7b5
K Lee <kevinle2997@gmail.com>
2019-02-19 09:48:58 +1300

    yonghui's improvements from 15th feb

--- DQN/Brain.py
+++ DQN/Brain.py
@@ -48,7 +48,7 @@ class Learning():
         # equation designed for training on 10 000 episodes
         # epsilon is below 0 until 'c' episodes is reached and is approx 1 for last 1000 episodes
         #  formula = 1 - a ** (-b * (episode - c))
-        self.epsilon = 1 - 1.2 ** (-0.003 * (episode - 4000))
+        self.epsilon = 1 - 1.2 ** (-0.003 * (episode - 2000))
         self.e_greedy_formula = 'e = 1-1.2^(-0.003*(episode-4000))'
         return action
 
