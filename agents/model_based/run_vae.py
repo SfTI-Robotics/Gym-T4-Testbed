96fe4236be70ca479fafc20f1c2a83c48874f2d7
Raymond Wang <raymond.wang.0214@gmail.com>
2019-07-09 19:03:21 +1200

    Fixed running vae

--- agents/model_based/run_vae.py
+++ agents/model_based/run_vae.py
@@ -11,7 +11,12 @@ print(image.shape)
 vae = VAE()
 vae.set_weights('./vae_weights.h5')
 
-predicted_image = vae.predict([image])
+# Need to give the input an extra dimension since model.predict() expects batch size as well [batch_size, img_height, img_width, channels]
+image = np.expand_dims(image,axis = 0)
+predicted_image = vae.predict(image)
 
-cv2.imwrite('./predicted.jpg', predicted_image)
+# Have to remove the extra layer and multiply rgb values by 255 to get the original image since before we divided by 255 during storage
+image_vector = predicted_image[0,:,:,:]*255
+
+cv2.imwrite('./predicted.jpg', predicted_image[0,:,:,:]*255)
 
